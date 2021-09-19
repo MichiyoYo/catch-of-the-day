848e97c2c7e0fec6c6388a92ebbbcc5f91a8ee04
Cristina Lester <cristina.deelester@gmail.com>
2021-09-19 15:34:37 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import StorePicker from "./components/StorePicker";
+import App from "./components/App";
+import "./css/style.css";
+
+const mainContainer = document.querySelector("#main");
+
+ReactDOM.render(<App />, mainContainer);
