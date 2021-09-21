bbcc0a7ffd2d5a7ff68d7df232e0a64ea55772a6
Cristina Lester <cristina.deelester@gmail.com>
2021-09-21 11:27:58 -0700

    implemented remove form order without synching with firebase

--- src/components/App.js
+++ src/components/App.js
@@ -65,6 +65,12 @@ class App extends React.Component {
     this.setState({ order: order });
   };
 
+  removeFromOrder = (key) => {
+    const order = { ...this.state.order };
+    delete order[key];
+    this.setState({ order: order });
+  };
+
   updateFish = (key, updatedFish) => {
     //1. take copy of the current state
     const fishes = { ...this.state.fishes };
@@ -99,7 +105,11 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
